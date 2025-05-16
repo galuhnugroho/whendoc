7317293270e7ea8169f337f9606eb722e7b45b5e
Galuh <gitgaluh@gmail.com>
2025-05-16 23:51:25 +0700

    make dashboard page controller

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\ObatController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ObatController;
+use App\Http\Controllers\DashboardController;
 
 Route::prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('obat', ObatController::class);
 });
